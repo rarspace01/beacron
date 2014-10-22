9f9e6509b63b66be90d0a3c1de32851a81c0c024
rarspace01 <rarspace01@gmail.com>
2014-10-22 22:25:09 +0200

    *ADD: -init code for insecur econnection over BT, starting with Android 2.3.3 (API10)

--- src/de/hamann/beacron/MainActivity.java
+++ src/de/hamann/beacron/MainActivity.java
@@ -1,12 +1,24 @@
 package de.hamann.beacron;
 
-import android.support.v7.app.ActionBarActivity;
+import java.io.IOException;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
-
+	private final static int REQUEST_ENABLE_BT = 1;
+	private static final String TAG = "Beacron";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,5 +42,80 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public boolean onHostButton(View view){
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+		    // Device does not support Bluetooth
+		}
+		
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		
+		Log.d(TAG,"Local BT:"+mBluetoothAdapter.getAddress());
+		
+		//mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(name, uuid)
+		
+		UUID testUUID = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+		
+		try{
+			BluetoothServerSocket listener = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord("TestDevice", testUUID);
+			BluetoothSocket socket = listener.accept();
+			
+			if(socket != null){
+				Log.d(TAG, "connection accepted");
+				Log.d(TAG,"Strength: "+socket.getRemoteDevice().EXTRA_RSSI);
+			} else {
+				Log.e(TAG, "no connection found");
+			}
+			
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		
+		return true;
+	}
+	
+	public boolean onConnectButton(View view){
+		
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+		    // Device does not support Bluetooth
+		}
+		
+		if (!mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		
+		//mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(name, uuid)
+		
+		UUID testUUID = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+		
+		try{
+			
+			BluetoothDevice remoteDevice = mBluetoothAdapter.getRemoteDevice("98:D6:F7:6E:9B:11");
+			
+			
+			BluetoothSocket socket= remoteDevice.createInsecureRfcommSocketToServiceRecord(testUUID);
+			
+			
+			if(socket != null){
+				Log.d(TAG, "connection accepted");
+				Log.d(TAG,"Strength Remote: "+socket.getRemoteDevice().EXTRA_RSSI);
+			} else {
+				Log.e(TAG, "no connection found");
+			}
+			
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		
+		return true;
 	}
 }
