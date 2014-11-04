44958dd3d0385bbd871abab9cc9f5857a713cdaf
rarspace01 <rarspace01@gmail.com>
2014-11-04 21:41:15 +0100

    *FIX: -reciever registration -working BT discovery

--- src/de/hamann/beacron/MainActivity.java
+++ src/de/hamann/beacron/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -16,11 +17,22 @@ import android.view.View;
 public class MainActivity extends ActionBarActivity {
 	private final static int REQUEST_ENABLE_BT = 1;
 	private static final String TAG = "Beacron";
-	
+
+	private BluetoothAdapter mBluetoothAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+      // Register for broadcasts when a device is discovered
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(mReceiver, filter);
+
+        // Register for broadcasts when discovery has finished
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filter);
+		
 	}
 
 	@Override
@@ -42,96 +54,144 @@ public class MainActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 	
-	public boolean onHostButton(View view){
-		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
-		    // Device does not support Bluetooth
-		}
-		
-		if (!mBluetoothAdapter.isEnabled()) {
-//		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-		    
-			Intent discoverableIntent = new
-			Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-			discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
-			startActivity(discoverableIntent);
-			
-		    
-		}else{
-			Intent discoverableIntent = new
-			Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-			discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
-			startActivity(discoverableIntent);
-		}
-		
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
-		
-		return true;
-	}
-	
-	public boolean onConnectButton(View view){
-		
-		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (mBluetoothAdapter == null) {
-		    // Device does not support Bluetooth
-		}
-		
-		if (!mBluetoothAdapter.isEnabled()) {
-		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-		}
-		
-        // If we're already discovering, stop it
-        if (mBluetoothAdapter.isDiscovering()) {
+        // Make sure we're not doing discovery anymore
+        if (mBluetoothAdapter != null) {
         	mBluetoothAdapter.cancelDiscovery();
         }
-		
+
+        // Unregister broadcast listeners
+        this.unregisterReceiver(mReceiver);
+    }
+
+	public boolean onHostButton(View view) {
+		BluetoothAdapter mBluetoothAdapter = BluetoothAdapter
+				.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			// Device does not support Bluetooth
+		}
+
+		if (!mBluetoothAdapter.isEnabled()) {
+			// Intent enableBtIntent = new
+			// Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			// startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+			Intent discoverableIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+			discoverableIntent.putExtra(
+					BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
+			startActivity(discoverableIntent);
+
+		} else {
+			Intent discoverableIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+			discoverableIntent.putExtra(
+					BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
+			startActivity(discoverableIntent);
+		}
+
+		return true;
+	}
+
+	public boolean onConnectButton(View view) {
+
+		//start BT stuff
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mBluetoothAdapter == null) {
+			// Device does not support Bluetooth
+		}
+
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+
+		// If we're already discovering, stop it
+		if (mBluetoothAdapter.isDiscovering()) {
+			mBluetoothAdapter.cancelDiscovery();
+		}
+
+		Log.d(TAG, "starting Discovery");
+
 		mBluetoothAdapter.startDiscovery();
 		
-        // Register for broadcasts when a device is discovered
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        this.registerReceiver(mReceiver, filter);
+		Log.d(TAG, "started Discovery");
 		
 		return true;
 	}
-	
-    // The BroadcastReceiver that listens for discovered devices and
-    // changes the title when discovery is finished
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-        	
-        	//Log.d()
-        	
-            String action = intent.getAction();
 
-            short rssiv=0;
-            
-            // When discovery finds a device
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Get the BluetoothDevice object from the Intent
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                
-                rssiv = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
-                
-               Log.d(TAG,"["+device.getAddress()+"] - ["+device.getName()+"] - "+rssiv);
-                
-                // If it's already paired, skip it, because it's been listed already
-                
-                
-                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-//                    mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                }
-            // When discovery is finished, change the Activity title
-            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-//                setProgressBarIndeterminateVisibility(false);
-//                setTitle(R.string.select_device);
-//                if (mNewDevicesArrayAdapter.getCount() == 0) {
-//                    String noDevices = getResources().getText(R.string.none_found).toString();
-//                    mNewDevicesArrayAdapter.add(noDevices);
-//                }
-            }
-        }
-    };
+//	private class RetrieveRouteTask extends AsyncTask<String, Void, Void> {
+//
+//		@Override
+//		protected Void doInBackground(String... params) {
+//			String deviceAddress = params[0];
+//			return null;
+//
+//			// start BT discovery here
+//			
+//
+//		}
+//
+//		protected void onPostExecute() {
+//			//after
+//		}
+//
+//	}
+
+	// The BroadcastReceiver that listens for discovered devices and
+	// changes the title when discovery is finished
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			String action = intent.getAction();
+			
+			Log.d(TAG,
+					"BT broadcast recieved Action:["+action+"]");
+			
+			short rssiv = 0;
+			boolean isFound = false;
+
+			// When discovery finds a device
+			if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+				// Get the BluetoothDevice object from the Intent
+				BluetoothDevice device = intent
+						.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+				rssiv = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,
+						Short.MIN_VALUE);
+
+				Log.d(TAG,
+						"[" + device.getAddress() + "] - [" + device.getName()
+								+ "] - " + rssiv);
+
+				// check for BT HW Adress
+
+				if (isFound) {
+
+					mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+					if (mBluetoothAdapter == null) {
+						// Device does not support Bluetooth
+					}
+
+					// If we're already discovering, stop it
+					if (mBluetoothAdapter.isDiscovering()) {
+						Log.d(TAG, "Stopping BT discovery");
+						mBluetoothAdapter.cancelDiscovery();
+					}
+					Log.d(TAG, "Starting BT discovery again");
+					mBluetoothAdapter.startDiscovery();
+
+				}
+
+			} else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED
+					.equals(action)) {
+				Log.d(TAG,"finished discovery");
+			}
+		}
+	};
 }
